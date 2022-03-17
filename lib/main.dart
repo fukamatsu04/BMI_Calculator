bc46b052afd6d40c1694c182e18b96c599762aa4
Isaac Silva <isaackun98@gmail.com>
2022-03-17 11:28:07 -0300

    Fixed the problem of screen rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'input_page.dart';
 import 'colors.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(BMICalculator());
 }
 
